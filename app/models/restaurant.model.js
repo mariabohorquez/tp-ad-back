413f2b61266c5086555c18d89e756ada9af0a33b
besednjak <besednjakmario@gmail.com>
2022-09-02 20:40:31 -0300

    cambio en modelo de restarurante

--- app/models/restaurant.model.js
+++ app/models/restaurant.model.js
@@ -1,18 +1,51 @@
 module.exports = mongoose => {
   const schema = mongoose.Schema(
     {
-      name: String,
-      type: String
+      name: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      openingTime:{
+        type: Date,
+        required: true,
+      },
+      closingTime:{
+        type: Date,
+        required: true,
+      },
+      priceRange:{
+        type: String,
+        required: true
+      },
+      address:{
+        streetName: String,
+        streetNumber: Integer,
+        neighborhood: String,
+        city: String,
+        state: String,
+        country: String,
+      },
+      categories:[
+        {
+          type:String,
+          required: true,
+        }
+      ],
+      menu:[{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'menu'
+      }]  
     },
     { timestamps: true }
-  );
-
-  schema.method("toJSON", function() {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
-  });
-
-  const Restaurant = mongoose.model("restaurant", schema);
-  return Restaurant;
-};
+    );
+    
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+    
+    const Restaurant = mongoose.model("restaurant", schema);
+    return Restaurant;
+  };
