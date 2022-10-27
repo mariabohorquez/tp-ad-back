732c9d54d296a717251f6473e8751d6f5c8086f3
Maria Bohorquez <gabriellabohorquez@gmail.com>
2022-10-27 12:03:51 -0300

    Add routes to user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -405,7 +405,11 @@ exports.delete = (req, res) => {
         message: 'Could not delete user with id=' + id + ' with error: ' + err
       })
     })
+<<<<<<< HEAD
 }
+=======
+  }
+>>>>>>> 918ccf8 (Add routes to user)
 
 // Upload a user image
 exports.uploadUserImage = (req, res) => {
